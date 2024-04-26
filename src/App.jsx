51d880abf107598bd0c7ab4087618cb4c4f51048
Harsh Vardhan <devharsh2k4@gmail.com>
2024-04-26 18:32:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import AboutUs from "./components/about";
+import Users from "./components/users";
+import Navbar from "./components/navbar";
+import NotFound from "./components/NotFound";
+import UserProfile from "./components/userProfile";
+import SearchUser from "./components/searchUser";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/user/:username" element={<UserProfile />} />
+          <Route path="/search"  element={<SearchUser/>}/>
+        
+        </Route>
+
+        <Route path="*" element={<NotFound/>}/>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
